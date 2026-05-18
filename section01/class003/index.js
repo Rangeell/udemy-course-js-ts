c78d198faf9f0d2c9e093255cd3c16503a412c28
Rangel <breno.r.ramos1@gmail.com>
2026-05-18 16:11:30 -0300

    add new class exercise

---
+++ section01/class003/index.js
@@ -0,0 +1,10 @@
+// Strings
+
+console.log('Breno "Rangel"') // String = Texto/Cadeia de caracteres
+console.log("Breno 'Rangel'") 
+console.log(`Breno 'Rangel' "Ramos"`)
+
+// Numbers
+console.log(123456) // Números inteiros
+console.log(35, 15.85, 'Breno') // Números inteiros, números reais (de ponto flutuante) e string
+
