0ad1de9cef29c834ba68c3feaa5ac443c0844b96
Rangel <breno.r.ramos1@gmail.com>
2026-05-19 16:45:56 -0300

    add new class

--- section02/class011/node_29e6e28b0840c.tmp.js
+++ section02/class011/node_29e6e28b0840c.tmp.js
@@ -6,8 +6,9 @@ const num3 = 10
 
 console.log(num1 * num2 / num3)
 
+let passo = 2
 
-for (let contador = 1; contador <= 10; contador += 2) {
+for (let contador = 1; contador <= 10; contador + passo) {
     console.log(`Número ${contador}`)
 }
 console.log('Fim do contador')
