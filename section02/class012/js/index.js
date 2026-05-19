4af9be722e885748b5123d6dcf78594f9f17ce26
Rangel <breno.r.ramos1@gmail.com>
2026-05-19 17:39:11 -0300

    add exercise

---
+++ section02/class012/js/index.js
@@ -0,0 +1,5 @@
+const n1 = Number(prompt('Digite um número'))
+const n2 = Number(prompt('Digite outro número'))
+let result = n1 + n2
+
+alert(`O 'resultado da soma entre ${n1} e ${n2} é ${result}!`)
