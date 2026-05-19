abad5e74c423e894b21cffae75c3537e1ad06259
Rangel <breno.r.ramos1@gmail.com>
2026-05-19 08:35:47 -0300

    reorganize files

---
+++ section02/class006/index.js
@@ -0,0 +1,17 @@
+/*
+
+let nome = 'João' // String
+
+console.log(`${nome} nasceu em 1984.`)
+console.log(`Em 2000 ${nome} conheceu Maria.`)
+console.log(`${nome} casou-se com Maria em 2012.`)
+console.log(`Maria teve 1 filho com ${nome} em 2015.`)
+console.log(`O filho de ${nome} se chama Eduardo.`)
+
+*/
+
+let nome; // Declarou variável
+nome = 'João' // Inicializou variável
+console.log(nome)
+nome = 'Otávio'
+console.log(nome)
