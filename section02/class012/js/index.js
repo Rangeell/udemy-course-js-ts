23ab2aaae06585137d17e365af6165bd32274ff2
Rangel <breno.r.ramos1@gmail.com>
2026-05-19 18:26:45 -0300

    add challenge from class 012

--- section02/class012/js/index.js
+++ section02/class012/js/index.js
@@ -1,5 +1,4 @@
 const n1 = Number(prompt('Digite um número'))
 const n2 = Number(prompt('Digite outro número'))
-let result = n1 + n2
 
-alert(`O 'resultado da soma entre ${n1} e ${n2} é ${result}!`)
+alert(`O 'resultado da soma entre ${n1} e ${n2} é ${n1 + n2}!`)
