8b8eccb0f24274a6c276d74b2074a6e66d83ffc0
Rangel <breno.r.ramos1@gmail.com>
2026-05-26 06:32:33 -0300

    att challenge from class023

--- section03/class023/challenge-imc/assets/js/main.js
+++ section03/class023/challenge-imc/assets/js/main.js
@@ -1,24 +1,29 @@
 (function myScope() {
     // Select DOM elements
+
+    let resulText
     const element = {
         form: document.querySelector('form'),
         weight: document.querySelector('#iweight'),
         height: document.querySelector('#iheight'),
-        divResult: document.querySelector('.res')
+        divResult: document.querySelector('.res'),
+        buttonClear: document.querySelector('button[type="reset"]')
     }
 
     element.form.addEventListener('submit', function (evento) {
         evento.preventDefault()
+        element.weight.focus()
         element.divResult.innerText = ''
 
         const weight = Number((element.weight.value))
         const height = Number((element.height.value))
 
-        const resulText = document.createElement('span')
+        resulText = document.createElement('span')
         const imc = (weight / (height ** 2)).toFixed(1)
 
-        let imcResult = 'Abaixo do peso'
+        let imcResult
         if (imc < 18.5) {
+            imcResult = 'Abaixo do peso'
         } else if (imc <= 24.9) {
             imcResult = 'Eutrófico'
         } else if (imc <= 29.9) {
@@ -34,4 +39,9 @@
         element.divResult.appendChild(resulText)
         resulText.innerText = `Seu IMC é ${imc} (${imcResult})`
     })
+
+    element.buttonClear.addEventListener('click', function clear() {
+        resulText.remove()
+    })
+
 })()
