aeef035e4b2e89f79512ff5b40c9908638e78dd9
Rangel <breno.r.ramos1@gmail.com>
2026-05-27 10:29:40 -0300

    add class026 exercises

---
+++ section03/class026/switch-case.js
@@ -0,0 +1,30 @@
+let diaSemana = new Date().getDay()
+
+
+
+switch (diaSemana) {
+    case 0:
+        diaSemana = 'Domingo'
+        break
+    case 1:
+        diaSemana = 'Segunda-feira'
+        break
+    case 2:
+        diaSemana = 'Terça-feira'
+        break
+    case 3:
+        diaSemana = 'Quarta-feira'
+        break
+    case 4:
+        diaSemana = 'Quinta-feira'
+        break
+    case 5:
+        diaSemana = 'Sexta feira'
+        break
+    case 6:
+        diaSemana = 'Sábado'
+        break
+    default: diaSemana = 'Dia inválido'
+}
+
+console.log(diaSemana)
---
+++ section03/class026/switch-function.js
@@ -0,0 +1,14 @@
+function getWeekDay(day) {
+    switch (day) {
+        case 0: return 'Domingo'
+        case 1: return 'Segunda-feira'
+        case 2: return 'Terça-feira'
+        case 3: return 'Quarta-feira'
+        case 4: return 'Quinta-feira'
+        case 5: return 'Sexta feira'
+        case 6: return 'Sábado'
+        default: 'Dia inválido'
+    }
+}
+const WeekDay = new Date().getDay()
+console.log(getWeekDay(WeekDay))
