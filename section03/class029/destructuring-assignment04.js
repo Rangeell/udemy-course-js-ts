a321272ef784a1b9d114a639727354dfc4192f65
Rangel <breno.r.ramos1@gmail.com>
2026-05-29 07:57:25 -0300

    small changes

--- section03/class029/destructuring-assignment04.js
+++ section03/class029/destructuring-assignment04.js
@@ -1,4 +1,3 @@
-// GETTTING REST
 //               0    1   2   3   4   5   6   7   8 
 const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90];
 const [one, , three, , five, , seven, ...rest] = numbers
