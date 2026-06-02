f231d22e6aa89dac3fe58fc3e6edf1597ed7b083
Rangel <breno.r.ramos1@gmail.com>
2026-06-02 17:07:42 -0300

    challenge from class042 solved

--- section03/class042-ex-timer/assets/js/main.js
+++ section03/class042-ex-timer/assets/js/main.js
@@ -2,41 +2,45 @@
 
     const timerContainer = document.querySelector('.timer')
     let seconds = 0
-    let stopWatch = 0
+    let timeInterval = null
+
+    function getTimeString(rawSeconds) {
+        const hours = padZero(Math.floor(rawSeconds / 3600))
+        const minutes = padZero(Math.floor(rawSeconds % 3600 / 60))
+        const seconds = padZero(Math.floor(rawSeconds % 60))
+
+        return `${hours}:${minutes}:${seconds}`
+    }
+
+    function padZero(time) {
+        return time.toString().padStart('2', 0)
+    }
 
     const startButton = document.querySelector('#start')
     startButton.addEventListener('click', function () {
-        clearInterval(stopWatch)
+        clearInterval(timeInterval)
 
         timerContainer.classList.remove('stop')
 
-        stopWatch = setInterval(function () {
-            console.log(seconds++)
+        timeInterval = setInterval(function () {
+            seconds++
+            const stopWatch = getTimeString(seconds)
+            timerContainer.innerText = stopWatch
         }, 1000)
     })
 
     const pauseButton = document.querySelector('#pause')
     pauseButton.addEventListener('click', function () {
         timerContainer.classList.add('stop')
-        clearInterval(stopWatch)
+        clearInterval(timeInterval)
     })
 
     const resetButton = document.querySelector('#reset')
     resetButton.addEventListener('click', function () {
-        timerContainer.classList.remove('stop')
-        clearInterval(stopWatch)
+        clearInterval(timeInterval)
         seconds = 0
+
+        timerContainer.innerText = '00:00:00'
+        timerContainer.classList.remove('stop')
     })
-
-    function formatTimer(time) {
-        return time.toString().padStart('2', 0)
-    }
-
-    function getTimer(rawSeconds) {
-        const hours = formatTimer(Math.floor(rawSeconds / 3600))
-        const minutes = formatTimer(Math.floor(rawSeconds % 3600 / 60))
-        const seconds = formatTimer(Math.floor(minutes % 60))
-
-        return `${hours}:${minutes}:${seconds}`
-    }
 })()
