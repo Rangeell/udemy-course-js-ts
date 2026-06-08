486ae55fce44c710203e2cec01087c2c91f8e229
Rangel <breno.r.ramos1@gmail.com>
2026-06-08 18:08:39 -0300

    add class 057 & exercises

---
+++ section05-arrays/class057-array-concatenation/concat01.js
@@ -0,0 +1,5 @@
+// Concatenando arrays o método tradicional com .concat()
+const a1 = [1, 2, 3]
+const a2 = [4, 5, 6]
+const a3 = a1.concat(a2, [7, 8, 9], 'Breno') // E pode continuar...
+console.log(a3)
