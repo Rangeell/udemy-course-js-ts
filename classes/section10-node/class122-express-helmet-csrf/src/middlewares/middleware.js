2f8b4a56a65cd2e4379b6b160a86bcf789fb04c3
Rangel <breno.r.ramos1@gmail.com>
2026-07-09 10:51:22 -0300

    add class122 & express (helmet & csrf) exercise

---
+++ classes/section10-node/class122-express-helmet-csrf/src/middlewares/middleware.js
@@ -0,0 +1,19 @@
+module.exports.middlewareGlobal = (req, res, next) => {
+    res.locals.umaVariavelLocal = 'Este é o valor da variável local.' // Disponível em todas as páginas
+    next()
+}
+
+module.exports.outroMiddleware = (req, res, next) => {
+    next()
+}
+
+module.exports.checkCsrfError = ((err, req, res, next) => {
+    if (err && err.code === 'EBADCSRFTOKEN') {
+        return res.render('404') // Se ocorrer o erro, renderizamos a página de erro
+    }
+})
+
+module.exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken()
+    next()
+}
