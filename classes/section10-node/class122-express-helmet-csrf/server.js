773c9281d4f57f31d63d86b608440715021486c3
Rangel <breno.r.ramos1@gmail.com>
2026-07-09 11:48:38 -0300

    class122 - add comments

--- classes/section10-node/class122-express-helmet-csrf/server.js
+++ classes/section10-node/class122-express-helmet-csrf/server.js
@@ -1,11 +1,10 @@
-require('dotenv').config() //  Injeta as variáveis do arquivo .env na memória do Node
+require('dotenv').config() // Injeta as variáveis do arquivo .env na memória do Node
+const express = require('express')  // Exportando express
+const path = require('path') // Exportado para facilitar o trabalho com caminhos de arquivos
 
-const express = require('express')
-const path = require('path')
+const app = express() // Executando o express
 
-const app = express()
-
-const mongoose = require('mongoose') // Chamando o mongoose (dependência do projeto)
+const mongoose = require('mongoose') // Chamando o mongoose (dependência do projeto) -> responsável por modelar a nossa base de dados (através de schemas)
 mongoose.connect(process.env.CONNECTIONSTRING)
     .then(() => {
         console.log('Conectei à base de dados!')
@@ -13,19 +12,18 @@ mongoose.connect(process.env.CONNECTIONSTRING)
     })
     .catch(e => console.log(e))
 
-const session = require('express-session') // Apenas isso já salva a sessão na memória
+const session = require('express-session') // Apenas isso já salva a sessão na memória -> Servem para identificar o navegador do cliente através de um cookie, permitindo que o servidor reconheça o usuário
 const MongoStore = require('connect-mongo')(session) // De cara já mandadmos (incomum) a sessão
-
-const flash = require('connect-flash') // Flash Massages
-const routes = require('./routes')
-const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware') // Nosso Middleware global e de csrf Error-> Atribuição via desestruturação
-const helmet = require('helmet')
-const csrf = require('csurf')
+const flash = require('connect-flash') // Flash Massages (menssagens "auto-destrutivas") -> Feedback para o usuário -> sem session, não funcionam
+const routes = require('./routes') // Rotas da nossa aplicação (home, contato, página inicial) caem aqui
+const helmet = require('helmet') // Recomendação do Express para deixar a nossa aplicação mais segura
+const csrf = require('csurf') // Faz com que todos os formulários tenham um crsf token. O que garante que nenhum site/app externo consiga postar coisas para dentro de nossa aplicação
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware') // Nossos Middleware global e de csrf Error-> Atribuição via desestruturação -> são funções que são executadas em cadeia no meio das rotas
 
 app.use(helmet())
-app.use(express.urlencoded({ extended: true })) // Middleware global -> 
-app.use(express.static(path.resolve(__dirname, 'public'))) // Middle global -> Avisa ao Express que a pasta 'public' contém os arquivos estáticos do site
-
+app.use(express.urlencoded({ extended: true })) // Middleware global -> Permite que postemos formulários para dentro de nossa aplicação
+app.use(express.json()) // 
+app.use(express.static(path.resolve(__dirname, 'public'))) // Middle global -> Avisa ao Express que a pasta 'public' contém os arquivos estáticos do site e permite acessá-los diretamente
 
 const sessionOptions = session({
     secret: process.env.SECRET,
@@ -39,8 +37,10 @@ const sessionOptions = session({
 })
 app.use(sessionOptions)
 app.use(csrf())
-app.use(csrfMiddleware)
 app.use(flash())
+
+// Chamando nossas rotas
+app.use(csrfMiddleware)
 app.use(middlewareGlobal) // Nosso middleware global -> todas as requisições, em todas as rotas e em todos os verbos vão passar por esse middleware
 app.use(checkCsrfError)
 app.use(routes)
