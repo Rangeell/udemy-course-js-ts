d7560cd636fd1e453d77534ed8223ce7be2318af
Rangel <breno.r.ramos1@gmail.com>
2026-07-20 16:23:37 -0300

    chore: clean up boilerplate and create first component
    - Remove unused default logos, styles, and service workers
    - Clean up index.html and index.js
    - Create basic App component with initial CSS reset

---
+++ classes/section14-React-Basic/to-do-list/src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react"; // Mesmo se não formos usar React neste aqui arquivo, precisamos importar o React porque vamos usar JSX
+import './App.css'
+
+export default function App() {
+  return <h1>Hello Wolrd!</h1>
+}
