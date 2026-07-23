4b7a7fb81afa28011c5839bcd5955cf3d656cdf5
Rangel <breno.r.ramos1@gmail.com>
2026-07-23 20:35:37 -0300

    feat(header): add navigation header with react-icons

--- classes/section15-React-Hooks-Redux/react-base/src/App.jsx
+++ classes/section15-React-Hooks-Redux/react-base/src/App.jsx
@@ -1,13 +1,15 @@
 // import { useState } from 'react';
 
+import Header from './components/Header';
 import Login from './pages/Login';
 import GlobalStyles from './styles/GlobalStyles'; // Dica: todo componente do React começa com letra maiúscula
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
-      <GlobalStyles/>
+      <GlobalStyles />
     </>
   );
 }
---
+++ classes/section15-React-Hooks-Redux/react-base/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import { Nav } from './styles';
+import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+
+export default function Header() {
+  return (
+    <Nav>
+      <a href=""><FaHome size={24} /></a>
+      <a href=""><FaSignInAlt size={24} /></a>
+      <a href=""><FaUserAlt size={24} /></a>
+    </Nav>
+  );
+}
