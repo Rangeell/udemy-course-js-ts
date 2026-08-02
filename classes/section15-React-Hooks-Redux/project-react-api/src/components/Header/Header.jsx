751a32daaca84979a579b9226af9701010a1c6ef
Rangel <breno.r.ramos1@gmail.com>
2026-08-02 15:26:20 -0300

    feat(class - section 15 (auth)): add logout functionality
    - Render power-off icon and status indicator when logged in
    - Clear auth state and token via loginFailure action on logout
    - Redirect user to home page after logging out

--- classes/section15-React-Hooks-Redux/project-react-api/src/components/Header/Header.jsx
+++ classes/section15-React-Hooks-Redux/project-react-api/src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { FaHome, FaSignInAlt, FaUserAlt, FaCircle, FaPowerOff } from 'react-icons/fa';
 import { Link } from 'react-router-dom'; // Substitui a tag HTML `<a>` para navegação interna, evitando o recarregamento da página.
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
+import * as actions from '../../store/modules/auth/actions';
 import { Nav } from './styles';
 
 export default function Header() {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  const navigate = useNavigate();
+  const dispath = useDispatch();
+
+  const handleLogout = e => {
+    e.preventDefault();
+    dispath(actions.loginFailure());
+    navigate('/');
+  };
+
   return (
     <Nav>
       <Link to="/" target=''>
@@ -14,9 +27,17 @@ export default function Header() {
         <FaUserAlt size={24} />
       </Link>
 
-      <Link to="/login">
-        <FaSignInAlt size={24} />
-      </Link>
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/login">
+          <FaPowerOff size={24} />
+        </Link>
+      ) : (
+        <Link to="/login">
+          <FaSignInAlt size={24} />
+        </Link>
+      )}
+
+      {isLoggedIn && <FaCircle size={24} color='#66ff33' />}
     </Nav >
   );
 }
