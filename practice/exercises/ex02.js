1a27b54c1c4832605369995dee4cbfb3a34dd78d
Rangel <breno.r.ramos1@gmail.com>
2026-08-03 11:26:58 -0300

    feat(practice - exercises): implement invertObject to swap keys and values

--- practice/exercises/ex02.js
+++ practice/exercises/ex02.js
@@ -1,5 +1,5 @@
 /*
-Desafio 1: O Contador de Ocorrências
+Exercise 02: Count Elements / Element Counter
 
 Objetivo:
     Escreva uma função chamada contarElementos que recebe um array de strings ou números e retorna um objeto onde cada chave é um elemento do array e o valor é a quantidade de vezes que ele aparece.
@@ -10,7 +10,7 @@ Regras / Requisitos:
     3. Deve funcionar com strings e números.
 */
 
-const contElements = function (arr) {
+const countElements = function (arr) {
     if (!Array.isArray(arr)) throw new TypeError('O argumento deve ser um array não vazio!');
     if (arr.length === 0) return {};
 
@@ -24,6 +24,6 @@ const contElements = function (arr) {
     return elements
 };
 
-console.log(contElements(["maçã", "banana", "maçã", "laranja", "banana", "maçã"]));
-console.log(contElements([1, 2, 2, 3, 1, 1]));
-console.log(contElements([]));
+console.log(countElements(["maçã", "banana", "maçã", "laranja", "banana", "maçã"]));
+console.log(countElements([1, 2, 2, 3, 1, 1]));
+console.log(countElements([]));
---
+++ practice/exercises/ex03.js
@@ -0,0 +1,36 @@
+/*
+Exercise 03: Invert Object
+
+Objetivo:
+    Escreva uma função chamada inverterObjeto que recebe um objeto e retorna um novo objeto onde as chaves viram valores e os valores viram chaves.
+
+Regras / Requisitos:
+    1. A função deve validar se o argumento é um objeto válido. Lembre-se de que em JavaScript, null e Arrays também retornam 'object' no typeof. Se não for um objeto válido, lance um TypeError.
+
+    2. Os valores do objeto original serão sempre strings ou numbers.
+
+    3. Se o objeto estiver vazio, retorne {}.
+*/
+
+const invertObject = function (obj) {
+    if (typeof obj !== 'object' || obj == null || Array.isArray(obj)) {
+        throw new TypeError('O argumento para a função deve ser um objeto!')
+    }
+    if (Object.keys(obj).length === 0) return {}
+
+    const newObject = Object.entries(obj).reduce((acc, v) => {
+        acc[v[1]] = v[0]
+
+        return acc
+    }, {})
+
+    return newObject
+}
+
+console.log(invertObject({}));
+
+console.log(invertObject({ a: "primeiro", b: "segundo", c: "terceiro" }));
+// Deve retornar: { primeiro: "a", segundo: "b", terceiro: "c" }
+
+console.log(invertObject({ nome: "Carlos", idade: 28 }));
+// Deve retornar: { Carlos: "nome", '28': "idade" }
