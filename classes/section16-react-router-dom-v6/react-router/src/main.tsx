587a7f0299edb325a1b4460599551537b3a92bbb
Rangel <breno.r.ramos1@gmail.com>
2026-08-04 16:41:11 -0300

    feat(class - section 16 - (router)): replace anchor tags with Link component and add Menu navigation

--- classes/section16-react-router-dom-v6/react-router/src/main.tsx
+++ classes/section16-react-router-dom-v6/react-router/src/main.tsx
@@ -5,10 +5,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/global.css';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Menu from './components/Menu/Menu';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
