b154de5d52185ab7fb9f84aa113c0f7f45bec477
Rangel <breno.r.ramos1@gmail.com>
2026-08-04 13:56:44 -0300

    feat(class - section 16 (router)): install react-router-dom v6 and setup base layout
    - Add react-router-dom@v6 dependency
    - Clean up Vite boilerplate
    - Create Home component structure
    - Add global CSS reset

--- classes/section16-react-router-dom-v6/react-router/eslint.config.js
+++ classes/section16-react-router-dom-v6/react-router/eslint.config.js
@@ -18,5 +18,15 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
+    rules: {
+      // 2. Regras estritas de estilo inspiradas no Airbnb:
+      semi: ["warn", "always"], // Obriga usar ponto e vírgula
+      quotes: ["warn", "single"], // Obriga usar aspas simples
+      "comma-dangle": ["error", "always-multiline"], // Obriga a vírgula no final de objetos/arrays multilinhas
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }], // Remove linhas em branco extras
+
+      // 3. Suas exceções para as aulas do professor Otávio:
+      "no-unused-vars": "warn", // Avisa sobre variáveis não usadas (linha amarela)
+    }
   },
 ])
