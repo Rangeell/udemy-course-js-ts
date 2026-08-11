f6e667dd67481001aa31d2f74fb9ad453e7fd555
Rangel <breno.r.ramos1@gmail.com>
2026-08-11 17:38:36 -0300

    chore(class/section 18 - Typescript): setup eslint with typescript support

---
+++ classes/section18-typescript/eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig({
+    files: ['**/*.{js,ts}'],
+    extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+    ],
+
+    rules: {
+        semi: ['error', 'always'], // Obriga usar ponto e vírgula
+        quotes: ['error', 'single'], // Obriga usar aspas simples
+        'comma-dangle': ['error', 'always-multiline'], // Obriga a vírgula no final de objetos/arrays multilinhas
+    },
+});
