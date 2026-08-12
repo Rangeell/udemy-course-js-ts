53bae296e8231b371a3ed60a13308af86656d5d2
Rangel <breno.r.ramos1@gmail.com>
2026-08-12 13:40:33 -0300

    docs(class/section 19 - Typescript - basics types): add explanation and examples for any type

---
+++ classes/section18-19-typescript-basics-types/src/class002-type-any/type-any.ts
@@ -0,0 +1,15 @@
+/*
+O tipo "any" representa uma falta de tipo ou "qualquer tipo"
+
+Normalmente não queremos esse tipo no nosso código, pois pode afetar a integridade promovendo falhas segurança / tipagem
+
+Utilize "any" apenas em último caso
+*/
+
+function showMessage(msg: any) { //
+  return msg;
+}
+
+console.log(showMessage([1, 2, 3]));
+console.warn(showMessage('Olá'));
+console.log(showMessage(2));
