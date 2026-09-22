46f4b8a06c072281eb8befbd820181cf7eaddb26
Rangel <breno.r.ramos1@gmail.com>
2026-09-22 16:29:29 -0300

    feat(lessons/section27 - TypeScript-React-Pomodoro-App): implement timer start/pause controls and working theme
    - Add timeCounting and working state management to PomodoroTimer
    - Integrate useInterval delay toggling for pause/play functionality
    - Add useEffect side effect to toggle 'working' class on document.body

--- lessons/section27-typescript-react-pomodoro-app/src/components/pomodoro-timer.tsx
+++ lessons/section27-typescript-react-pomodoro-app/src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useInterval } from '../hooks/useInterval';
 import { Button } from './button';
 import { Timer } from './timer';
@@ -13,30 +13,45 @@ interface PomodoroProps {
 
 export function PomodoroTimer({ pomodoroTime }: PomodoroProps) { // Destructuring
   const [mainTime, setMainTime] = useState(pomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false); // Verifica se está contando ou não
+  const [isWorking, setIsWorking] = useState(false);
+
+  useEffect(() => {
+    if (isWorking) { //! Não é indicado manipular o DOM diretamente
+      document.body.classList.add('working');
+    }
+  }, [isWorking]);
 
   // Nosso hook personalizado
   useInterval(() => {
     // Usando o state updater (prevTime) do hook useState nativo do React
     setMainTime(prevTime => prevTime - 1);
-  }, 1000);
+  }, timeCounting ? 1000 : null); // Se for null, o contador não roda
+
+  const configureWork = () => {
+    setTimeCounting(true);
+    setIsWorking(!isWorking);
+  };
 
   return (
-    <div className="pomodoro">
+    <div className='pomodoro'>
       <h2>You are: Working</h2>
       <Timer mainTime={mainTime} />
 
       <div className='controls'>
         <Button
-          text='teste'
-          onClick={() => console.log(1)}>
+          text='Work'
+          onClick={() => configureWork()}>
         </Button>
+
         <Button
           text='teste'
           onClick={() => console.log(1)}>
         </Button>
+
         <Button
-          text='teste'
-          onClick={() => console.log(1)}>
+          text={timeCounting ? 'Pause' : 'Play'}
+          onClick={() => setTimeCounting(!timeCounting)}>
         </Button>
       </div>
 
